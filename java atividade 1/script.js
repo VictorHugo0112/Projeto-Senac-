51a6b0afdd5c4807123fdaa23e4d1e7c3ba4311d
VictorHugo0112 <Victorhugo01122010@gmail.com>
2026-04-29 22:40:49 -0300

    Update script.js

--- java atividade 1/script.js
+++ java atividade 1/script.js
@@ -82,3 +82,60 @@ function telementos() {
     telementos2.textContent = "Frieren";
     telementos3.textContent = "Fern";
 }
+function atividadedificil () {
+    let dificil = document.getElementById("paragrafodificil");
+    dificil.textContent = "Mudei";
+    dificil.style.color = "blue";
+    dificil.style.fontSize = "50px";
+}
+let botao = document.getElementById("botao");
+
+botao.addEventListener("click", function() {
+    let valor = document.getElementById("campoTexto").value;
+
+    document.getElementById("saida1").innerHTML = valor;
+    document.getElementById("saida2").innerHTML = valor;
+});
+function color () {
+    let color = document.getElementById("afetado");
+    color.style.color = "aqua";
+}
+function size () {
+    let size = document.getElementById("afetado");
+    size.style.fontSize = "50px";
+}
+function text () {
+    let text = document.getElementById("afetado");
+    text.textContent = "ferrou";
+}
+function div() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    div1.style.color = "red";
+    div2.style.color = "blue";
+}
+let botao7 = document.getElementById("botao7");
+
+botaocopia.addEventListener("click", function() {
+    let valorcopia = document.getElementById("campoTextocopia").value;
+    let paragrafocopia = document.getElementById("paragrafocopia");
+
+    paragrafocopia.innerHTML = valor;
+    paragrafocopia.style.color = "blue";
+});
+function somaelimpa() {
+    let num3 = Number(document.getElementById("num3").value);
+    let num4 = Number(document.getElementById("num4").value);
+    let resultado2 = num3 + num4;
+    let local1 = document.getElementById("resultado2");
+    local1.textContent = resultado2;  
+   } 
+   function limparsoma() {
+    let resultado3 = document.getElementById("resultado2");
+    resultado3.textContent = "";
+   }
+   function mudartudo() {
+    let mudetudo = document.getElementById("background")
+    mudetudo.style.color = "orange"
+    mudetudo.style.fontSize = "50px"
+   }
